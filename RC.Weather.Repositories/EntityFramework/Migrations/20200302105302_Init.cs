9e8706216c0b77776c7f41c48548a2e52228df12
Aaron Urkin <a.urkin@outlook.com>
2020-03-02 13:37:13 +0200

    EF Migration has been added. To create dababase please run 'Update-Database -StartupProject Hosting\RC.Weather.Api' command within Package Manager Console with RC.Weather.Repositories project selected

---
+++ RC.Weather.Repositories/EntityFramework/Migrations/20200302105302_Init.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RC.Weather.Repositories.EntityFramework.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Conditions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CityCode = table.Column<string>(nullable: true),
+                    Temperature = table.Column<double>(nullable: false),
+                    WeatherText = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conditions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Favorites",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CityCode = table.Column<string>(nullable: true),
+                    CityName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Favorites", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conditions_CityCode",
+                table: "Conditions",
+                column: "CityCode",
+                unique: true,
+                filter: "[CityCode] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Favorites_CityCode",
+                table: "Favorites",
+                column: "CityCode",
+                unique: true,
+                filter: "[CityCode] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Conditions");
+
+            migrationBuilder.DropTable(
+                name: "Favorites");
+        }
+    }
+}
